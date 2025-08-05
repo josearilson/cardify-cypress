ac18a7b3e1269799a4f11da49063d2baeb5d5694
José Arilson Dias Ramalho <jose.ramalho@spread.com.br>
2025-08-05 12:27:50 -0300

    envio de projeto cardify-cypress

---
+++ src/pages/AddCard.cy.jsx
@@ -0,0 +1,100 @@
+import React from 'react'
+import AddCard from './AddCard'
+
+
+Cypress.Commands.add('alertErrorHaveText', (expectdText) => {
+
+  cy.contains('.alert-error', expectdText)
+    .should('be.visible')
+
+})
+
+
+Cypress.Commands.add('fillCardForm', (myCard) => {
+
+  cy.get('[data-cy="number"]').type(myCard.number)
+  cy.get('[data-cy="holderName"]').type(myCard.holderName)
+  cy.get('[data-cy="expirationDate"]').type(myCard.expirationDate)
+  cy.get('[data-cy="cvv"]').type(myCard.cvv)
+
+  cy.get(`[data-cy="bank-${myCard.bank}"]`).click()
+})
+
+Cypress.Commands.add('submitCardForm', () => {
+  cy.get('[data-cy="saveMyCard"]').click()
+
+})
+describe('<AddCard />', () => {
+
+  const myCard = {
+    number: '347974535272064',
+    holderName: 'Jose Arilson',
+    expirationDate: '12/35',
+    cvv: '123',
+    bank: 'nubank'
+  }
+  beforeEach(() => {
+
+    cy.viewport(1400, 900)
+    cy.mount(<AddCard />)
+
+  })
+
+  it('exibe erros quando os campos não são informados', () => {
+
+    cy.submitCardForm()
+
+    const alerts = [
+      'Número do cartão é obrigatório',
+      'Data de expiração é obrigatória',
+      'CVV é obrigatório',
+      'Selecione um banco'
+    ]
+
+    alerts.forEach((alert) => {
+      cy.alertErrorHaveText(alert)
+    })
+
+  })
+
+  it('Deve cadastrar um novo cartao de credito', () => {
+
+    cy.fillCardForm(myCard)
+    // Intercepta as requisições GET .
+    cy.intercept('POST', 'http://wallet.cardfify.dev/api/cards', {
+      statusCode: 201,
+      body: myCard
+    }).as('addCard');
+    
+    cy.submitCardForm()    
+    cy.wait('@addCard');
+    
+    cy.contains('.notice-success', 'Cartão cadastrado com sucesso!').should('be.visible');
+
+  })
+
+  it('Valida campo de nome titular com menos de 2 caracteres', () => {
+
+    cy.fillCardForm({ ...myCard, holderName: 'J' })      
+    cy.submitCardForm()
+    cy.alertErrorHaveText('Nome deve ter pelo menos 2 caracteres')
+
+  })
+
+  it('Valida data de expiracao invalida', () => {
+
+    cy.fillCardForm({ ...myCard, expirationDate: '13/35' }) 
+    cy.submitCardForm()
+    cy.alertErrorHaveText('Data de expiração inválida ou vencida')
+
+  })
+
+  it('Valida cvv com menos de 3 digitos', () => {
+
+    cy.fillCardForm({ ...myCard, cvv: '12' })
+    cy.submitCardForm()
+    cy.alertErrorHaveText('CVV deve ter 3 ou 4 dígitos')
+
+  })
+
+})
